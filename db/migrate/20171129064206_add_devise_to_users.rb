5de5de436cb2e25316da1a2c357efe3be974a79e
pinkg023 <pinkg023@gmail.com>
2017-12-06 09:56:27 +0800

    t.timestamps null: false

--- db/migrate/20171129064206_add_devise_to_users.rb
+++ db/migrate/20171129064206_add_devise_to_users.rb
@@ -33,7 +33,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
 
       # Uncomment below if timestamps were not included in your original model.
       t.string :role
-      #t.timestamps null: false
+      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
